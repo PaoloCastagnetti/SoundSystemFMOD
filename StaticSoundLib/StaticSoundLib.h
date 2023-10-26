3d69e1142bed3a3627644b94db6b615d444c31e6
Paolo Castagnetti <castagnettipaolo01@gmail.com>
2023-10-26 11:50:26 +0200

    Added documentation comments

--- StaticSoundLib/StaticSoundLib.h
+++ StaticSoundLib/StaticSoundLib.h
@@ -16,12 +16,14 @@ public:
 	 * @return Boolean value indicating the success of the initialization.
 	 */
 	bool init();
+
 	/**
 	 * @brief Releases the resources used by the Sound system.
 	 *
 	 * @return Boolean value indicating the success of the release operation.
 	 */
 	bool release();
+
 	/**
 	 * @brief Updates the Sound system.
 	 *
@@ -29,12 +31,33 @@ public:
 	 */
 	bool update();
 
+	/**
+	 * @brief Plays the provided FMOD sound using the Sound System.
+	 *
+	 * @param sound Pointer to the FMOD Sound object to be played.
+	 * @return Boolean value indicating the success of playing the sound.
+	 */
 	bool playSound(FMOD::Sound *sound);
 
+	/**
+	 * @brief Sets the looping mode for the currently playing sound to continuous looping.
+	 *
+	 * @return Boolean value indicating the success of setting the looping mode.
+	 */
 	bool setLoopOn();
 
+	/**
+	 * @brief Disables the looping mode for the currently playing sound.
+	 *
+	 * @return Boolean value indicating the success of disabling the looping mode.
+	 */
 	bool setLoopOff();
 
+	/**
+	 * @brief Stops the currently playing sound.
+	 *
+	 * @return Boolean value indicating the success of stopping the sound.
+	 */
 	bool stop();
 
 	FMOD::System* system;
